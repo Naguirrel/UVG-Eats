ca0b8aa3be354896642c349a144b0e8f5dde910b
Pablownski <sal24374@uvg.edu.gt>
2024-10-17 18:18:05 -0600

    login 1.2

--- Login.java
+++ Login.java
@@ -17,3 +17,32 @@ public class Login {
             System.out.println("El correo electrónico ya está en uso.");
         }
     }
+    public boolean iniciarSesion(String correoElectronico, String contrasena) {
+        if (usuariosRegistrados.containsKey(correoElectronico)) {
+            Usuario usuario = usuariosRegistrados.get(correoElectronico);
+            if (usuario.verificarContrasena(contrasena)) {
+                usuarioActual = usuario;
+                System.out.println("Inicio de sesión exitoso.");
+                return true;
+            } else {
+                System.out.println("Contraseña incorrecta.");
+            }
+        } else {
+            System.out.println("Correo electrónico no encontrado.");
+        }
+        return false;
+    }
+
+    public void cerrarSesion() {
+        if (usuarioActual != null) {
+            System.out.println("Sesión cerrada para " + usuarioActual.getCorreoElectronico());
+            usuarioActual = null;
+        } else {
+            System.out.println("No hay usuario activo.");
+        }
+    }
+
+    public Usuario getUsuarioActual() {
+        return usuarioActual;
+    }
+}
