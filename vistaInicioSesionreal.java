838699099a1da7739db78e317aa7d70283506eca
Juan Salguero <sal24374@uvg.edu.gt>
2024-10-30 22:20:05 -0600

    1.2

--- vistaInicioSesionreal.java
+++ vistaInicioSesionreal.java
@@ -21,3 +21,32 @@ public class vistaInicioSesionreal {
         listaUsuarios.add(new Usuario(1, "Juan Salguero", "sal24374@uvg.edu.gt", "Jpsa241105"));
         listaUsuarios.add(new Usuario(2, "Jane Doe", "jane@example.com", "mypassword"));
     }
+    public void mostrarFormulario(JFrame frame) {
+        frame.getContentPane().removeAll();
+        frame.repaint();
+        
+        Image iconImage = new ImageIcon(this.getClass().getResource("img1/IMG_3273.png")).getImage();
+        frame.setIconImage(iconImage);
+
+        JLabel imgTwitter = new JLabel("");
+        Image imgTwit = new ImageIcon(this.getClass().getResource("img1/X.png")).getImage();
+        imgTwitter.setIcon(new ImageIcon(imgTwit));
+        imgTwitter.setBounds(180, 25, 50, 50);
+        frame.getContentPane().add(imgTwitter);
+        
+        JLabel imgInsta = new JLabel("");
+        Image imgIg = new ImageIcon(this.getClass().getResource("img1/ig.png")).getImage();
+        imgInsta.setIcon(new ImageIcon(imgIg));
+        imgInsta.setBounds(110, 25, 50, 50);
+        frame.getContentPane().add(imgInsta);
+
+        JLabel anun = new JLabel("¡Visítanos en redes sociales!");
+        anun.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 15));
+        anun.setBounds(87, 80, 180, 36);
+        frame.getContentPane().add(anun);
+
+        JLabel logoImageLabel = new JLabel();
+        Image logoImage = new ImageIcon(this.getClass().getResource("img1/uvg150.png")).getImage();
+        logoImageLabel.setIcon(new ImageIcon(logoImage));
+        logoImageLabel.setBounds(100, 150, 125, 100);
+        frame.getContentPane().add(logoImageLabel);
